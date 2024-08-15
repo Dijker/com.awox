21ec47656cac23228fc1c80fc9f0a1614b13a96f
Geurt <Geurt@Dijker.nu>
2024-08-15 14:07:46 +0200

    Bump 0.1.0

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ class MyApp extends Homey.App {
    */
   async onInit() {
     this.log('MyApp has been initialized');
+    this.homey.notifications.createNotification({
+      excerpt : 'AwoX App Stops! Migrate to Eglo App! \nPlease Read: https://community.homey.app/t/63043/23'
+    });
   }
-
 }
 
 module.exports = MyApp;
